10cef1fa6822d5375cddd14e05e3cb390f0ff590
John Oberschelp <JohnOberschelp@gmail.com>
2025-03-09 15:34:20 -0700

    Changed paths from VS2019 to VS2022

--- StateSort/race.bat
+++ StateSort/race.bat
@@ -1,7 +1,7 @@
 rem  The following 3 lines contain paths that my be different on your PC
-set VC=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC
+set VC=C:\Program Files\Microsoft Visual Studio\2022\Community\VC
 call "%VC%\Auxiliary\Build\vcvars64.bat"
-"%VC%\Tools\MSVC\14.29.30133\bin\Hostx64\x64\CL" /O2 race.cpp
+"%VC%\Tools\MSVC\14.42.34433\bin\Hostx64\x64\CL" /O2 race.cpp
 DEL race.obj
 race
 pause
